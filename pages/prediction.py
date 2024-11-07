39ab251210ef7a2fe625c1fcfa6142627884b8c6
RofiifSyarof <rofiifsyarof@gmail.com>
2024-11-07 21:23:31 +0700

    Update File

--- pages/prediction.py
+++ pages/prediction.py
@@ -1,7 +1,7 @@
 # pages/prediksi.py
 import streamlit as st
 import pickle
-import numpy as np
+import pandas as pd
 
 # Load the trained model
 model_path = 'model/gradient_boost_model.pkl'
@@ -15,31 +15,36 @@ st.title("Prediksi Tingkat Kesuburan Tanah")
 st.write("Masukkan nilai nutrisi tanah untuk melakukan prediksi:")
 
 # Formulir input untuk setiap fitur tanah
-N = st.number_input("Rasio Nitrogen (NH4+) dalam tanah", min_value=0.0, step=0.1)
-P = st.number_input("Rasio Fosfor (P) dalam tanah", min_value=0.0, step=0.1)
-K = st.number_input("Rasio Kalium (K) dalam tanah", min_value=0.0, step=0.1)
-ph = st.number_input("Keasaman Tanah (pH)", min_value=0.0, step=0.1)
-ec = st.number_input("Kapasitas Konduktivitas Listrik", min_value=0.0, step=0.1)
-oc = st.number_input("Kandungan Karbon Organik", min_value=0.0, step=0.1)
-S = st.number_input("Kandungan Sulfur (S)", min_value=0.0, step=0.1)
-zn = st.number_input("Kandungan Seng (Zn)", min_value=0.0, step=0.1)
-fe = st.number_input("Kandungan Besi (Fe)", min_value=0.0, step=0.1)
-cu = st.number_input("Kandungan Tembaga (Cu)", min_value=0.0, step=0.1)
-Mn = st.number_input("Kandungan Mangan (Mn)", min_value=0.0, step=0.1)
-B = st.number_input("Kandungan Boron (B)", min_value=0.0, step=0.1)
+col1,  col2 = st.columns([1, 1], vertical_alignment="center")
+with col1:
+    N = st.number_input("Rasio Nitrogen (NH4+) dalam tanah", min_value=0.0, step=0.1)
+    P = st.number_input("Rasio Fosfor (P) dalam tanah", min_value=0.0, step=0.1)
+    K = st.number_input("Rasio Kalium (K) dalam tanah", min_value=0.0, step=0.1)
+    ph = st.number_input("Keasaman Tanah (pH)", min_value=0.0, step=0.1)
+    ec = st.number_input("Kapasitas Konduktivitas Listrik", min_value=0.0, step=0.1)
+    oc = st.number_input("Kandungan Karbon Organik", min_value=0.0, step=0.1)
+with col2:
+    S = st.number_input("Kandungan Sulfur (S)", min_value=0.0, step=0.1)
+    zn = st.number_input("Kandungan Seng (Zn)", min_value=0.0, step=0.1)
+    fe = st.number_input("Kandungan Besi (Fe)", min_value=0.0, step=0.1)
+    cu = st.number_input("Kandungan Tembaga (Cu)", min_value=0.0, step=0.1)
+    Mn = st.number_input("Kandungan Mangan (Mn)", min_value=0.0, step=0.1)
+    B = st.number_input("Kandungan Boron (B)", min_value=0.0, step=0.1)
 
 # Tombol untuk melakukan prediksi
 if st.button("Prediksi"):
+    # Definsikan nama kolom yang sesuai dengan pelatihan model
+    feature_names = ['N', 'P', 'K', 'pH', 'EC', 'OC', 'S', 'Zn', 'Fe', 'Cu', 'Mn', 'B']
     # Mempersiapkan data input untuk model
-    input_data = np.array([[N, P, K, ph, ec, oc, S, zn, fe, cu, Mn, B]])
+    input_data = pd.DataFrame([[N, P, K, ph, ec, oc, S, zn, fe, cu, Mn, B]], columns=feature_names)
     
     # Melakukan prediksi
     prediction = model.predict(input_data)[0]
 
     # Menampilkan hasil prediksi
     if prediction == 0:
-        st.success("Prediksi: Tanah Tidak Subur")
-    elif prediction == 1:
         st.success("Prediksi: Tanah Kurang Subur")
-    else:
+    elif prediction == 1:
         st.success("Prediksi: Tanah Subur")
+    else:
+        st.success("Prediksi: Tanah Sangat Subur")
